3fc6b7adce655526ad8e97f13242a714c613b850
Aleco112 <aleco112@yahoo.fr>
2022-04-21 00:58:24 +0000

    first-part

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
@@ -8,5 +11,26 @@ import "../styles/index.css";
 //import your own components
 import Layout from "./layout";
 
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apikey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.measurementId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/js/utilities/signIn.js
@@ -0,0 +1,12 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+export const signIn = (email, password)=>{
+    return signInWithEmailAndPassword (getAuth(), email, password)
+     .then((userCredential) => {
+        console.log(userCredential)
+       
+    }) .catch((error) => {
+       console.log(error.message)
+    });
+}
